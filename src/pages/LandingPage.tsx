941f99064e8922e966eb364b0b03863b49267320
rakafebriansy <kerenr445@gmail.com>
2024-09-22 12:08:56 +0700

    fix: remove three comment

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -120,8 +120,8 @@ const LandingPage:React.FC  = () => {
                     <img src={aboutImg} className="w-full" alt="" />
                 </div>
             </section>
-            {/* <section id="three-model" ref={mountRef} className="w-full overflow-hidden">
-            </section> */}
+            <section id="three-model" ref={mountRef} className="w-full overflow-hidden">
+            </section>
             <section id="maps" className="p-10 flex flex-col gap-6 lg:gap-10 w-full items-center">
                 <h1 className="font-bold text-2xl lg:text-3xl text-center">Maps dan Peta Widamar</h1>
                 <div className="flex flex-col lg:grid lg:grid-cols-4 gap-4 lg:gap-8">
